f676015241531d7849185153fac966b217f97e46
Николай Пронин <Proninkolian@yandex.ru>
2024-07-06 19:10:52 +0300

    Changed styles and refactoring code

--- bodyshedule/Models/Event.cs
+++ bodyshedule/Models/Event.cs
@@ -8,12 +8,18 @@ namespace bodyshedule.Models
     {
         [Key] 
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Это поле является обязательным"), MaxLength(31)]
         public string Name { get; set; }
+
         [Required(ErrorMessage = "Это поле является обязательным"), MaxLength(60)]
         public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM H:mm}")]
         [Required(ErrorMessage = "Это поле является обязательным")]
         public DateTime StartTime { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM H:mm}")]
         [Required(ErrorMessage = "Это поле является обязательным")]
         public DateTime EndTime { get; set; }
 
